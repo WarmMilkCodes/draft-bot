427bb5fb049f5e02b8c5789a1ca8bd807f195f9d
WarmMilkCodes <warmmilkgames@gmail.com>
2024-09-11 22:31:09 -0500

    uppercase "Season"

--- cogs/draft.py
+++ cogs/draft.py
@@ -18,7 +18,7 @@ class Draft(commands.Cog):
     async def start_draft(self, ctx):
         draft_channel = self.bot.get_channel(config.bot_testing_channel)
         if draft_channel:
-            await draft_channel.send(f"The United Rogue League of Legends draft for season {LOL_season} is starting...")
+            await draft_channel.send(f"The United Rogue League of Legends draft for Season {LOL_season} is starting...")
             await ctx.respond("Draft Notification Sent.", ephemeral=True)
         else:
             await ctx.respond("Draft channel not found.", ephemeral=True)
